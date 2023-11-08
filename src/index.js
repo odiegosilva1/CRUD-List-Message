b728714b09aab8a9baffa768e1a0a5b05da8d98a
Diego Silva <odiegosilvadev@gmail.com>
2023-11-07 21:39:09 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-app.get('/', (request, response) => {
-return response.json('Server rodando');
+app.get('/', (req, res) => {
+return res.json('Server rodando');
 
 });
+
+// app.post('/user', function (req, res) {
+//     res.post('Insira um carro')
+// });
+// app.put('/user/:id', function (req, res) {
+//     res.send(`Editar um carro ${req.params.id}`);
+//   });
+   
+//   app.delete('/user/:id', function (req, res) {
+//     res.send(`Excluir um carro com o id ${req.params.id}`);
+//   });
 
 app.listen(8080, () => console.log("Servidor iniciado"));
