68a8f6bb29dd1f29a0b9d83c275029390ccf702d
Diego Silva <odiegosilvadev@gmail.com>
2023-11-07 23:19:19 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
-import express from 'express';
+import express, { request, response } from 'express';
 const app = express();
 app.use(express.json());
+
+let carros =[]
+let i = 1
+
 
 app.get('/', (req, res) => {
 return res.json('Server rodando');
 
 });
 
-// app.post('/user', function (req, res) {
-//     res.post('Insira um carro')
-// });
-// app.put('/user/:id', function (req, res) {
-//     res.send(`Editar um carro ${req.params.id}`);
-//   });
-   
-//   app.delete('/user/:id', function (req, res) {
-//     res.send(`Excluir um carro com o id ${req.params.id}`);
-//   });
+app.post('/user', function (req, res) {
+    res.post('Insira um user')
+});
+ app.post('/carros', (req, res)=> {
+    const infoRequest = request.body
+
+    const novoCarro = {
+        id: i++,
+        modelo: infoRequest.modelo,
+        marca: infoRequest.marca,
+        ano: infoRequest.ano,
+        cor:infoRequest.cor,
+        preco:infoRequest.preco
+    }
+    carros.push(novoCarro) {
+        return.res.status(201).json(carros)
+    }
+    carros.push(novoCarro);
+    return response.json('Ok Turma')
+ });
 
 app.listen(8080, () => console.log("Servidor iniciado"));
